609f9a47f44ea376cc28c407d955e1e8472552cf
chaudharymac <jaitraining10@gmail.com>
2024-10-15 16:52:32 -0400

    Moive_Review_Code

---
+++ src/main/java/chaudhary/jai/Movies/MovieService.java
@@ -0,0 +1,24 @@
+package chaudhary.jai.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findByImdbId (String imdbId){
+        return movieRepository.findByImdbId(imdbId);
+    }
+
+}
